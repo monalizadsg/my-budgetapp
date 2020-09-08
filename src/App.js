cd800d6b8de610c3a04ea284df4411b98dfe57b1
monalizadsg <monalizadasig@gmail.com>
2020-09-08 21:56:17 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+// import { Route, Switch } from "react-router-dom";
+import Header from "./components/common/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <h1>Hello world</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
