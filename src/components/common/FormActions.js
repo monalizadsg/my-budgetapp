de4942a845c7de7aaf152fa22b7c4b698d53aa2d
Monaliza Dasig <monalizadasig@gmail.com>
2020-11-15 13:45:24 +0800

    Created component for Form Action Buttons

---
+++ src/components/common/FormActions.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import Loading from "../common/Loading";
+
+const FormActions = ({
+  isEditing,
+  onClose,
+  isLoading,
+  onShowDialog,
+  onSubmit,
+}) => {
+  return (
+    <>
+      <div className='form-actions'>
+        {!isEditing ? (
+          <Button onClick={onClose} color='primary' disabled={isLoading}>
+            Cancel
+          </Button>
+        ) : (
+          <Button onClick={onShowDialog} color='primary' disabled={isLoading}>
+            Delete
+          </Button>
+        )}
+        <Button onClick={onSubmit} color='primary' disabled={isLoading}>
+          {!isEditing ? "Add" : "Save"}
+          <Loading isLoading={isLoading} size={18} />
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default FormActions;
