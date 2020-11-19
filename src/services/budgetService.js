d99cfb8aa299e1996c7c00aaa13250d17738910f
Monaliza Dasig <monalizadasig@gmail.com>
2020-11-19 09:25:13 +0800

    Add endpoints for  update and delete  budget

--- src/services/budgetService.js
+++ src/services/budgetService.js
@@ -16,4 +16,12 @@ export async function createBudget(data) {
   return await axios.post(`${url}/api/v1/budgets`, data);
 }
 
+export async function updateBudget(data, budgetId) {
+  return await axios.put(`${url}/api/v1/budgets/${budgetId}`, data);
+}
+
+export async function deleteBudget(budgetId) {
+  return await axios.delete(`${url}/api/v1/budgets/${budgetId}`);
+}
+
 export default { getBudgets, getBudgetBalances, createBudget };
