b559e388634806ad37acd8f446fdd06f9f2466f7
Monaliza Dasig <monalizadasig@gmail.com>
2021-03-18 09:49:25 +0800

    Override success color with my theme color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
   palette: {
     primary: { 500: "#22aa8d" },
+    success: { main: "#1dc292" },
   },
 });
 export default theme;
